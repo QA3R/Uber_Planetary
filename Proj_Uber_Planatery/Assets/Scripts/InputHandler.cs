dd69c55d097c28079478a90922e0bfaab3e76b83
FTWRahul <r.poundarik@gmail.com>
2020-07-30 05:33:55 -0400

    Some tests

---
+++ Proj_Uber_Planatery/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public delegate void RotationDelegate(Vector3 dir);
+
+    public delegate void MovementDelegate();
+
+    public RotationDelegate rotationDelegate;
+    public MovementDelegate movementDelegate;
+    
+    private void Update()
+    {
+        rotationDelegate?.Invoke(new Vector3(Input.GetAxis("Pitch"), Input.GetAxis("Yaw"), Input.GetAxis("Roll")));
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            movementDelegate?.Invoke();
+        }
+    }
+}
---
+++ Proj_Uber_Planatery/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private InputHandler _inputHandler;
+
+    public float rotationSpeed;
+    public float movementSpeed;
+
+    private Rigidbody _rigidbody;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _inputHandler = GetComponent<InputHandler>();
+        _inputHandler.rotationDelegate += Rotate;
+        _inputHandler.movementDelegate += Move;
+    }
+
+    private void Rotate(Vector3 dir)
+    {
+        transform.Rotate(dir * (rotationSpeed * Time.deltaTime));
+    }
+
+    private void Move()
+    {
+        _rigidbody.velocity = transform.forward * (movementSpeed * Time.deltaTime);
+        // transform.Translate(transform.forward * (movementSpeed * Time.deltaTime), Space.World);
+    }
+}
