906ad102ca59bbb7cd20af075df88707480dfd14
FTWRahul <r.poundarik@gmail.com>
2020-08-01 17:16:44 -0400

    Movement Parameters exposed

--- Proj_Uber_Planatery/Assets/Scripts/InputHandler.cs
+++ Proj_Uber_Planatery/Assets/Scripts/InputHandler.cs
@@ -7,22 +7,30 @@ public class InputHandler : MonoBehaviour
 {
     public delegate void RotationDelegate(Vector3 dir);
 
-    public delegate void MovementDelegate();
+    public delegate void MovementDelegate(float val);
+    public delegate void BoostDelegate();
 
     public RotationDelegate rotationDelegate;
     public MovementDelegate movementDelegate;
+    public BoostDelegate boostDelegate;
+
+    [SerializeField]private string xRotAxisName;
+    [SerializeField]private string yRotAxisName;
+    [SerializeField]private string zRotAxisName;
     
     private void Update()
     {
-        rotationDelegate?.Invoke(new Vector3(Input.GetAxis("Pitch"), Input.GetAxis("Yaw"), Input.GetAxis("Roll")));
-        
-    }
+        rotationDelegate?.Invoke(new Vector3(Input.GetAxisRaw(xRotAxisName), Input.GetAxisRaw(yRotAxisName), Input.GetAxisRaw(zRotAxisName)).normalized);
+
+        movementDelegate?.Invoke(Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetButton("Jump"))
+        {
+            boostDelegate?.Invoke();
+        }
+    }    
 
     private void FixedUpdate()
     {
-        if (Input.GetButton("Jump"))
-        {
-            movementDelegate?.Invoke();
-        }
     }
 }
--- Proj_Uber_Planatery/Assets/Scripts/PlayerController.cs
+++ Proj_Uber_Planatery/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +10,7 @@ public class PlayerController : MonoBehaviour
 
     public float rotationSpeed;
     public float movementSpeed;
+    public float boostSpeed;
 
     private Rigidbody _rigidbody;
     private void Awake()
@@ -17,6 +19,9 @@ public class PlayerController : MonoBehaviour
         _inputHandler = GetComponent<InputHandler>();
         _inputHandler.rotationDelegate += Rotate;
         _inputHandler.movementDelegate += Move;
+        _inputHandler.boostDelegate += Boost;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Rotate(Vector3 dir)
@@ -24,9 +29,14 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(dir * (rotationSpeed * Time.deltaTime));
     }
 
-    private void Move()
+    private void Move(float val)
     {
-        _rigidbody.velocity = transform.forward * (movementSpeed * Time.deltaTime);
-        // transform.Translate(transform.forward * (movementSpeed * Time.deltaTime), Space.World);
+        // _rigidbody.velocity = transform.forward * (val * (movementSpeed * Time.deltaTime));
+        transform.Translate(transform.forward * (val * (movementSpeed * Time.deltaTime)), Space.World);
+    }
+
+    private void Boost()
+    {
+        transform.Translate(transform.forward * (boostSpeed * Time.deltaTime), Space.World);
     }
 }
