f66668a6101c7e2a4802f5f3a6d3cba31eb2e936
FTWRahul <r.poundarik@gmail.com>
2020-08-13 15:25:08 -0400

    Shockwave Shader and animation

---
+++ Proj_Uber_Planetary/Assets/_Project/Scripts/Core/RotateOverTimeOnAxis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace UberPlanetary.Core
+{
+    public class RotateOverTimeOnAxis : MonoBehaviour
+    {
+        //[SerializeField] private bool x, y, z;
+        [SerializeField] [Range(0,1)] private float xMultiplier, yMultiplier, zMultiplier;
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            transform.Rotate(new Vector3(xMultiplier, yMultiplier, zMultiplier) * (speed * Time.deltaTime));
+        }
+    }
+}
