6129006525f42ee4413d3d3d3a2c739cefe2a7de
FTWRahul <r.poundarik@gmail.com>
2020-08-14 11:27:39 -0400

    Trails
    Trails
    PostProcessing
    Materials

---
+++ Proj_Uber_Planetary/Assets/_Project/Scripts/SetTrailEmmition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace UberPlanetary
+{
+    public class SetTrailEmmition : MonoBehaviour
+    {
+
+        private TrailRenderer _trail;
+
+        private void Awake()
+        {
+            _trail = GetComponent<TrailRenderer>();
+        }
+
+        public void SetEmitting(float val)
+        {
+            _trail.emitting = val > 0;
+        }
+    }
+}
