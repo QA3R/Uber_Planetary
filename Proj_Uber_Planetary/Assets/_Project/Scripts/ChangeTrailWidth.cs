cc8b44b5ac97403c2f2f089aac497c763f4e26a3
FTWRahul <r.poundarik@gmail.com>
2020-08-18 21:36:34 -0400

    Added curve

--- Proj_Uber_Planetary/Assets/_Project/Scripts/ChangeTrailWidth.cs
+++ Proj_Uber_Planetary/Assets/_Project/Scripts/ChangeTrailWidth.cs
@@ -8,6 +8,7 @@ namespace UberPlanetary
     {
         private TrailRenderer _trail;
         [SerializeField] private float minStarWidth, maxStartWidth, minEndWidth, maxEndWidth;
+        [SerializeField] private AnimationCurve curve;
 
         private void Awake()
         {
@@ -16,8 +17,8 @@ namespace UberPlanetary
 
         public void SetWidth(float val)
         {
-            _trail.startWidth = val.Remap(0, 1, minStarWidth, maxStartWidth);
-            _trail.endWidth = val.Remap(0, 1, minEndWidth, maxEndWidth);
+            _trail.startWidth = curve.Evaluate(val.Remap(0, 1, minStarWidth, maxStartWidth));
+            _trail.endWidth = curve.Evaluate(val.Remap(0, 1, minEndWidth, maxEndWidth));
         }
     }
 }
