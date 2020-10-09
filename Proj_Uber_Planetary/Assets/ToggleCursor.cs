41229e51967e305c624593548fb97d3fcb3aa31b
QA3R <46014750+QA3R@users.noreply.github.com>
2020-10-09 07:03:13 -0400

    Added Cursor unlock on MainMenu_Scene and reduced Playership speed.

---
+++ Proj_Uber_Planetary/Assets/ToggleCursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCursor : MonoBehaviour
+{
+    // Very code, much wow...
+    private void Awake()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
