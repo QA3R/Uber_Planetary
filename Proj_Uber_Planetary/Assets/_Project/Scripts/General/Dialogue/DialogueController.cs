2c3c72febfabac59f684c57941714a414951236e
Davi Sampaio <davi.gamedev@gmail.com>
2020-10-17 17:54:42 -0400

    First dialogue controller refactoring

--- Proj_Uber_Planetary/Assets/_Project/Scripts/General/Dialogue/DialogueController.cs
+++ Proj_Uber_Planetary/Assets/_Project/Scripts/General/Dialogue/DialogueController.cs
@@ -7,33 +7,38 @@ using UnityEngine.UI;
 
 namespace UberPlanetary.Dialogue
 {
+    /// <summary>
+    /// Controls dialogue flow and translates customer data into UI
+    /// </summary>
     public class DialogueController : MonoBehaviour
     {
+        //private members
         private RideManager _rideManager;
         private float _timeBetweenDialogue;
-        [SerializeField]private float autoPlayDialogueTime;
+        private TextAnimatorPlayer textAnimatorPlayer;
+        private int _lineIndex;
+        private int _characterIndex;
 
-        // Dialogue Box objects
+        //exposed fields
+        [SerializeField]private float autoPlayDialogueTime;
+        [SerializeField] private CustomerSO customerSO;
+        [SerializeField] private DialogueSO dialogueSO;
+
+        // public fields
         public TextMeshProUGUI custName;
         public TextMeshProUGUI dialogueBox;
         public GameObject dialogueWindow;
         public Image custFace;
-
         public TextAnimator textAnimator;
-        private TextAnimatorPlayer textAnimatorPlayer;
-
-        [SerializeField] private CustomerSO customerSO;
-        [SerializeField] private DialogueSO dialogueSO;
         public DialogueTrigger dialogueTrigger;
-
         public bool isStarted;
-        public bool IsShowing { get; set; }
-
-        public bool hasDialogue => dialogueSO != null;
-
         public string characterTalking;
-        private int _lineIndex;
-        private int _characterIndex;
+
+        //public properties
+        public bool IsShowing { get; set; }
+        public bool HasDialogue => dialogueSO != null;
+
+
 
         private void Awake()
         {
@@ -48,7 +53,13 @@ namespace UberPlanetary.Dialogue
             _rideManager.onCustomerPickedUp.AddListener(StartDialogue);
             _rideManager.onCustomerDroppedOff.AddListener(EndDialogue);
         }
-
+        //Turns the dialogue box on and off
+        public void ToggleDialogueBox(bool state)
+        {
+            dialogueWindow.SetActive(state);
+            dialogueBox.gameObject.SetActive(state);
+        }
+        //pulls customer information from other scripts toggles dialogue window on
         public void StartDialogue(CustomerSO customerData)
         {
             customerSO = customerData;
@@ -57,37 +68,20 @@ namespace UberPlanetary.Dialogue
 
             InitiateDialogue();
         }
-
-        public void EndDialogue(CustomerSO customerData)
+        //populates the window with customer information and begins running dialogue lines
+        public void InitiateDialogue()
         {
-            ClearCustomerData();
-            ClearDialogueBox();
-            FinishDialogue();
-            _lineIndex = 0;
-        }
+            isStarted = true;
+            custName.text = customerSO.CustomerName;
+            custFace.color = new Color(1, 1, 1, 1);
+            custFace.sprite = customerSO.CustomerFace;
+            textAnimatorPlayer.ShowText(dialogueSO.dialogueLines[_lineIndex++].line);
 
-        public void ToggleDialogueBox(bool state)
-        {
-            dialogueWindow.SetActive(state);
-            dialogueBox.gameObject.SetActive(state);
-        }
-
-        public void ClearDialogueBox()
-        {
-            dialogueBox.text = "";
-        }
-
-        public void ClearCustomerData()
-        {
-            customerSO = null;
-            dialogueSO = null;
-            custName.text = null;
-            custFace.sprite = null;
         }
 
         private void Update()
         {
-            if (IsShowing || !isStarted || !hasDialogue) return;
+            if (IsShowing || !isStarted || !HasDialogue) return;
 
             if (Input.GetKeyDown(KeyCode.F))
             {
@@ -96,7 +90,10 @@ namespace UberPlanetary.Dialogue
                     FinishDialogue();
                     return;
                 }
+                custName.text = dialogueSO.dialogueLines[_lineIndex].characterName;
+                custFace.sprite = dialogueSO.dialogueLines[_lineIndex].characterSpeaking;
                 DisplayText(dialogueSO.dialogueLines[_lineIndex++].line);
+
             }
             _timeBetweenDialogue = Mathf.Clamp(_timeBetweenDialogue, 0,autoPlayDialogueTime);
             if (_timeBetweenDialogue >= autoPlayDialogueTime)
@@ -107,33 +104,40 @@ namespace UberPlanetary.Dialogue
                     return;
                 }
                 DisplayText(dialogueSO.dialogueLines[_lineIndex++].line);
-                //_timeBetweenDialogue = 0;
             }
             else
             {
                 _timeBetweenDialogue += Time.deltaTime;
             }
         }
-
-        public void InitiateDialogue()
-        {
-            isStarted = true;
-            custName.text = customerSO.CustomerName;
-            custFace.color = new Color(1, 1, 1, 1);
-            custFace.sprite = customerSO.CustomerFace;
-            textAnimatorPlayer.ShowText(dialogueSO.dialogueLines[_lineIndex++].line);
-            
-        }
-
+        //plays text with typewriter effect
         public void DisplayText(string textToDisplay)
         {
             textAnimatorPlayer.ShowText(textToDisplay);
             _timeBetweenDialogue = 0;
-            custName.text = dialogueSO.dialogueLines[_lineIndex-1].characterName;
-            custFace.sprite = dialogueSO.dialogueLines[_lineIndex-1].characterSpeaking;
-            //Debug.Log(_lineIndex + " comparing to: " + dialogueSO.lines.Length);
+        }
+        //called at the end of the ride to run dialogue end methods
+        public void EndDialogue(CustomerSO customerData)
+        {
+            ClearCustomerData();
+            ClearDialogueBox();
+            FinishDialogue();
+            _lineIndex = 0;
         }
 
+        public void ClearDialogueBox()
+        {
+            dialogueBox.text = "";
+        }
+        
+        public void ClearCustomerData()
+        {
+            customerSO = null;
+            dialogueSO = null;
+            custName.text = null;
+            custFace.sprite = null;
+        }
+        //toggles dialogue box off and sets dialogue condition back to false
         public void FinishDialogue()
         {
             isStarted = false;
