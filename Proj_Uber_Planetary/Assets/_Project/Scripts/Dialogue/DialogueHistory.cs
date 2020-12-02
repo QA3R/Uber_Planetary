8d04b38b93347fa3fa6ef4b5a2c47ce59a4ed3b3
QA3R <46014750+QA3R@users.noreply.github.com>
2020-12-02 01:25:55 -0500

    Updates to the DialogueHistory.cs, Systems_Scene and PhoneController prefab

--- Proj_Uber_Planetary/Assets/_Project/Scripts/Dialogue/DialogueHistory.cs
+++ Proj_Uber_Planetary/Assets/_Project/Scripts/Dialogue/DialogueHistory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UberPlanetary.ScriptableObjects;
@@ -12,18 +13,34 @@ namespace UberPlanetary.Dialogue
         /// <summary>
         /// This script is responsible for populating the dialogue history.
         /// </summary>
+
         #region Variables
         private TextMeshProUGUI _dialogueLine;
         private TextMeshProUGUI _characterDialogue;
         private List <string> _dialogueList;
+        private Action <int> _timePaused;
+        private Action <int> _timeUnpaused;
 
-        [SerializeField] private GameObject screenBorder;
+
+        [SerializeField] private GameObject canvas;
         [SerializeField] private RectTransform dialogueHistory;
         [SerializeField] private GameObject dialoguePrefab;
         [SerializeField] private DialogueController dialogueController;
         [SerializeField] private Transform dialogueContainer;
+        #endregion
 
+        #region Properties
+        public Action <int> TimePaused 
+        {
+            get => _timePaused;
+            set => _timePaused = value;
+        }
 
+        public Action <int> TimeUnpaused 
+        {
+            get => _timeUnpaused;
+            set => _timeUnpaused = value;
+        }
         #endregion
 
         #region OnEnable, and OnDisable Methods
@@ -52,16 +69,24 @@ namespace UberPlanetary.Dialogue
 
         public void OpenDialogueHistory()
         {
-            screenBorder.SetActive(true);
+            canvas.SetActive(true);
             Cursor.visible = true;
-            Time.timeScale = 0;
+            
+            if (TimePaused != null)
+            {
+                TimePaused(0);
+            }
         }
 
         public void CloseDialogueHistory()
         {
-            screenBorder.SetActive(false);
+            canvas.SetActive(false);
             Cursor.visible = false;
-            Time.timeScale = 1;
+            
+            if (TimeUnpaused != null)
+            {
+                TimeUnpaused(1);
+            }
         }
         #endregion
     }
