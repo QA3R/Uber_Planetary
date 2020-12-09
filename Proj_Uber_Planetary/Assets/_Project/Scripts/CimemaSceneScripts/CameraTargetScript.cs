c6285a52128bbf16307a4061c2989798f16cbf54
Benedict Ahunu-Kumi <47798179+BenedictAK@users.noreply.github.com>
2020-12-08 21:19:09 -0500

    Created Cinamachine Timeline with 3 out of 5 cuts

---
+++ Proj_Uber_Planetary/Assets/_Project/Scripts/CimemaSceneScripts/CameraTargetScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTargetScript : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
