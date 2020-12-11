a5536bde7b9ff4dfefd989d9787939f97dfc298c
Rahul Poundarik <r.poundarik@gmail.com>
2020-12-11 07:43:40 -0800

    Cutscene load level and subtitles done

---
+++ Proj_Uber_Planetary/Assets/_Project/Scripts/LoadMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UberPlanetary
+{
+    public class LoadMainMenu : MonoBehaviour
+    {
+        public float timeToLoad;
+        
+        private void Start()
+        {
+            StartTransition(timeToLoad);
+        }
+
+        public void StartTransition(float time)
+        {
+            Invoke(nameof(GoToMenu), time);
+        }
+        
+        private void GoToMenu()
+        {
+            SceneManager.LoadScene("MainMenu_Scene");
+        }
+    }
+}
---
+++ Proj_Uber_Planetary/Assets/_Project/Scripts/PlaySubtitleText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Febucci.UI;
+using UnityEngine;
+
+namespace UberPlanetary
+{
+    public class PlaySubtitleText : MonoBehaviour
+    {
+        public TextAnimatorPlayer textAnimator;
+
+        [TextArea]
+        public string textToPlay;
+        
+        
+        private void Awake()
+        {
+            textAnimator = GetComponent<TextAnimatorPlayer>();
+            PlayText();
+        }
+
+        public void PlayText()
+        {
+            textAnimator.ShowText(textToPlay);
+        }
+    }
+}
